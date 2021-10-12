86b7dc40923cd7c0ef82e33a541a666288347419
Joshua Reyes <j_reyes314@yahoo.com>
2021-10-12 18:47:36 -0400

    Added relevant data for date and weather

--- src/App.js
+++ src/App.js
@@ -9,20 +9,19 @@ import WeatherCard from './components/weatherCard';
 // var date = new Date().getDay();
 
 function App() {
-
   const [isloading, setLoading] = useState(true);
   const [weather, setWeather] = useState([]);
-var date = new Date().getDay();
+  var date = new Date().getDay();
 
 
   console.log(date);
   useEffect(() => {
     const fetchData = ( async () => {const result = await axios.get('http://api.openweathermap.org/data/2.5/forecast?id=5128638&units=imperial&appid=d35e9be47f58e6445fb0227b42fdeed2')
       .then(function (response) {      
-        setWeather(response.data.list);
+        setWeather(response.data);
         setLoading(false);
 
-        console.log(response.data.list[5].main);
+        console.log(response.data);
       })})
     
       fetchData()
@@ -34,9 +33,10 @@ var date = new Date().getDay();
   if(isloading){
     
   }else{
+    arr.push(<h2>{weather.city.name}</h2>)
     
     for(let i =5; i < 40; i+=8){
-      arr.push( <WeatherCard weather={weather[i]} date = {date++}/> )
+      arr.push( <WeatherCard weather={weather.list[i]} date = {date++}/> )
     }
   }
 
@@ -45,7 +45,7 @@ var date = new Date().getDay();
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>5-day Forecast</h1>
         {arr}
       </header>
 
--- src/components/weatherCard.js
+++ src/components/weatherCard.js
@@ -2,13 +2,17 @@ import { useState } from "react";
 
 export default function WeatherCard (props) {
     const weather = props.weather;
-    console.log(props.date);
+    console.log(weather);
     const[week, setWeek] = useState(['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday', 'Sunday'])
     console.log(week[props.date])
 
     return(
-        
+        <div>
+            <h3>{week[props.date]}</h3>
+       <p>{weather.main.temp} &deg;F</p>
+
+            <h6>{weather.weather[0].description}</h6>
+        </div>
        
-       <p>{weather.main.temp}</p>
     )
 }
